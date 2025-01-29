65b74dd08ec938f72a3d8be609f831c0bf8a86a1
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-01-29 16:32:45 +0100

    Update 000_run_all_pipeline_in_one_script.sh

--- 00_scripts/000_run_all_pipeline_in_one_script.sh
+++ 00_scripts/000_run_all_pipeline_in_one_script.sh
@@ -6,7 +6,8 @@
 #time nohup bash 02_trimmomatic_q30.sh &> 02_trimmomatic_q30.out
 #time nohup bash 03_check_quality_of_cleaned_data.sh &> 03_check_quality_of_cleaned_data.out
 time nohup bash 05_qiime2_import_PE.sh &> 05_qiime2_import_PE.out
-time nohup bash 06_qiime2_denoise_PE.sh &> 06_qiime2_denoise_PE.out
+time nohup bash 06_deblur_denoise_SE.sh &> 06_deblur_denoise_SE.out
+#time nohup bash 06_qiime2_denoise_PE.sh &> 06_qiime2_denoise_PE.out
 time nohup bash 07_qiime2_tree_PE.sh &> 07_qiime2_tree_PE.out
 #time nohup bash 08_qiime2_rarefaction_PE.sh &> 08_qiime2_rarefaction_PE.out
 #time nohup bash 09_qiime2_calculate_and_explore_diversity_metrics_PE.sh &> 09_qiime2_calculate_and_explore_diversity_metrics_PE.out
